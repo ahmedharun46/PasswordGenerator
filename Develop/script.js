5019730f452de865399252b9d8c5d9cb62ada332
Hodman Ahmed <hodman003@Hodmans-MacBook-Air.local>
2022-07-24 10:48:18 -0500

    Sunday Commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,61 +1,102 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var ChosenCharacters;
+var ChosenCharacters = [];
+var passwordLength
+var Characters
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
-function chooseLength(){
-var passwordLength = window.prompt ("Please enter a number 8-128, to vhose how many characters you want in your password");
-window.alert("Your pass word length is " + passwordLength);
-} 
-chooseLength();
+function generatePassword() {
+  passwordLength = window.prompt("Please enter a number 8-128, to vhose how many characters you want in your password");
+  window.alert("Your pass word length is " + passwordLength);
 
-var UppercaseLetters = window.confirm ("Would you like Uppers Case Letter");
-if (UppercaseLetters){
-  window.alert("You choose a UpperCase Letter");
-  ChosenCharacters += "ABC";
-}
-if (UppercaseLetters===false){
-  window.alert("Did not choose UpperCase Letter");
-}
+  var UppercaseLetters = window.confirm("Would you like Uppers Case Letter");
+  if (UppercaseLetters) {
+    window.alert("You choose a UpperCase Letter");
+    var UppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    ChosenCharacters.push(...UppercaseLetters);
+    console.log(ChosenCharacters);
 
-var LowerCaseLetters = window.confirm ("Would you like a Lower Case Letter");
-if (LowerCaseLetters){
-  window.alert("You choose a LowerCasse Letter");
-  ChosenCharacters += "abc";
-}
-if (LowerCaseLetters===false){
+  }
+  if (UppercaseLetters === false) {
+    window.alert("Did not choose UpperCase Letter");
+  }
+
+
+  var LowerCaseLetters = window.confirm("Would you like a Lower Case Letter");
+  if (LowerCaseLetters) {
+    window.alert("You choose a LowerCasse Letter");
+    var LowerCaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    ChosenCharacters.push(...LowerCaseLetters);
+    console.log(ChosenCharacters);
+
+  }
+  if (LowerCaseLetters === false) {
     window.alert("Did not choose LowerCase Letter")
-   
+  }
+
+
+  var Numbers = window.confirm("Would you like a Number");
+  if (Numbers) {
+    window.alert("You choose a Number");
+    var Numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20','21','22','23','24','25','26','27','28','29','30'];
+    ChosenCharacters.push(...Numbers);
+    console.log(ChosenCharacters);
+
+  }
+  if (Numbers === false) {
+    window.alert("Did not choose a Number");
+  }
+
+
+  var SpecialCharacters = window.confirm("Would you like a Special Characters");
+  if (SpecialCharacters) {
+    window.alert("You choose a Special Character");
+    var SpecialCharacters = ['@', '!', '#', '$', '^', '&', '*', '(', ')'];
+    ChosenCharacters.push(...SpecialCharacters);
+    console.log(ChosenCharacters); 1
+  }
+  if (SpecialCharacters === false) {
+    window.alert("Did not choose Special Character");
+  }
+  function Randomizer() {
+  //Scramble Characters from the global array
+  for (var i = 0; i < passwordLength; i++) {
+    var Random = Math.floor(Math.random() * ChosenCharacters.length);
+    //console.log(ChosenCharacters[Random])
+    Characters+=ChosenCharacters[Random]
+    console.log(Characters.length)
+    console.log(passwordLength)
+    if(Characters.length==passwordLength){
+     window.alert("Here is your passwrod" + Characters)
+     console.log("Test")
+    }
+  }
+}
 }
 
-var Numbers = window.confirm ("Would you like a Number");
-if (Numbers){
-window.alert("You choose a Number");
-ChosenCharacters += "123";
-}
-if (Numbers===false){
-  window.alert("Did not choose a Number");
-} 
+//Randomizer()
+//return Characters
 
 
-var SpecialCharacters = window.confirm ("Would you like a Special Characters");
-if(SpecialCharacters){
-  window.alert("You choose a Special Character");
-  ChosenCharacters += "@!#"
-}
-  if (SpecialCharacters===false){
-  window.alert("Did not choose Special Character");
-}
+//if (passwordLength<8){
+//  window.alert("Invalid");
+//}
+//if (passwordLength>128){
+// window.alert("Invalid");
+//}
 
 
 
 
+
+
+// Returns number of Characters user selected 
 
 
 
