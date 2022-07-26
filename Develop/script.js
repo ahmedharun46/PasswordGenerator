197c18d36adf5122bd70666b31d656c366d97d2c
Hodman Ahmed <hodman003@Hodmans-MacBook-Air.local>
2022-07-25 19:48:09 -0500

    Second Final commit

--- Develop/script.js
+++ Develop/script.js
@@ -82,8 +82,6 @@ function generatePassword() {
   //Scramble Characters from the global array
   for (var i = 0; i < passwordLength; i++) {
     var Random = Math.floor(Math.random() * ChosenCharacters.length);
-  //console.log(ChosenCharacters[Random]);
-    //console.log(passwordLength);
   ScrambledPassword += ChosenCharacters[Random]
   console.log(ScrambledPassword);
     }
@@ -92,34 +90,6 @@ function generatePassword() {
   Randomizer()
   return ReturnPassword
 }
-
-
-
-//Randomizer()
-//return ChosenCharacters
-
-
-//if (passwordLength<8){
-//  window.alert("Invalid");
-//}
-//if (passwordLength>128){
-// window.alert("Invalid");
-//}
-
-
-
-
-
-
-// Returns number of Characters user selected 
-
-
-
-
-
-
-
-
 
 
 
@@ -147,31 +117,5 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-// declare function generatePassword()
-//variables holding the different characters
-//uppercase= ["A","B","C"]
-
-// I need to use prompt() to gather password criteria
-//prompt("How many characters would you linke in your password")
-// if(passwordLength <8) stop function
-// if(passwordLength >128) stop function
-// Add event listener to generate button
-//confirm()returns a boolean true/false
-//confirm() for uppercase letters
-// confirm() for lowercase letters
-//cofrim() for special characters
-//confrim() for numeric characters
-// once each cinfrim/prompt has been asnwered
-// if confrims are ture keep or get charcter type 
-// if no confirm is true stop function
-//if a confirm is true the concat related array to selected characters array
-// i.e ['A','B','C',a,b,c,1,2,3,%,$,#]
-//for loop based on password length prompt
-// use Math.floor (Math.random()* selectedCharacters.length)to get random index of slectedCharacters
-
-//var selectedCharacters= [];
-//var uppercaseCharacters= ['A', 'B','C'];
-//if(isUpperscase) {
-//selectedCharacters.concat(uppercaseCharacters)
 
 
